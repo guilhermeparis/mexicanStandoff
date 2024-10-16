1b4a0893346be389ec570a6a649b1415af800e52
Guilherme Paris <g.paris@edu.pucrs.br>
2024-10-16 00:35:02 -0300

    Adicionado método para receber argumentos pela linha de comando

--- mexicanStandoff.java
+++ mexicanStandoff.java
@@ -1,7 +1,24 @@
-public class mexicanStandoff
+public class MexicanStandoff
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        // Verifica se o número correto de argumentos foi passado
+        if (args.length != 3)
+        {
+            System.out.println("Deve passar exatamente 3 números inteiros como argumento.");
+            return;
+        }
+        try
+        {
+            // Converte os argumentos para inteiros
+            int n1 = Integer.parseInt(args[0]);
+            int n2 = Integer.parseInt(args[1]);
+            int n3 = Integer.parseInt(args[2]);
+            // Exibe os números digitados
+            System.out.println("Valores: " + n1 + ", " + n2 + ", " + n3);
+            
+        } catch (NumberFormatException e) {
+            System.out.println("Erro: Todos os argumentos devem ser números inteiros.");
+        }
     }
 }
